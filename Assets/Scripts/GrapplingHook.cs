ba501d94d414a5a3ef565957fcaa837ac54fd332
Dennis Nyberg <dennis.nyberg36@hotmail.se>
2020-09-08 16:06:44 +0200

    GrapplingHook build-on

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -9,7 +9,10 @@ using UnityEngine;
 public class GrapplingHook : MonoBehaviour
 {
     public Transform cam;
-    public GameObject bullet;
+    public GameObject Player;
+    public bool isHooked = false;
+    public Vector3 hookPosition;
+    public Vector3 playerToHook;
 
     [Header("Grappling Hook Stats")]
     [SerializeField]
@@ -21,19 +24,50 @@ public class GrapplingHook : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            GrapplingHookHandler();
+            if (!isHooked)
+            {
+                GrapplingHookHandler();
+            }
+            else
+            {
+                isHooked = false;
+            }
         }
+        if (isHooked)
+        {
+            
+            UnityEngine.Debug.Log("isHooked true and working");
+            Vector3 playerPosition = new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z);
+            playerToHook = (hookPosition - playerPosition);
 
+            //if(playerToHook.magnitude > 5) - kommentard ut, minimum distance så player inte hamnar i väggen.
+
+            Player.GetComponent<Rigidbody>().AddForce(playerToHook * HookSpeed, ForceMode.Impulse);
+
+        }
     }
 
     public void GrapplingHookHandler()
     {
         Vector3 playerPosition = new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z);
         UnityEngine.Debug.DrawRay(playerPosition, cam.transform.forward * 100, Color.red, 2f);
+        Ray ray = new Ray(playerPosition, cam.transform.forward);
 
-        GameObject clone;
+        RaycastHit hit;
+        if(Physics.Raycast (ray, out hit))
+        {
+            hookPosition = hit.point;
+            isHooked = true;
+        }
+
+
+
+        /*GameObject clone;
         clone = Instantiate(bullet, playerPosition, Quaternion.identity);
         clone.GetComponent<Rigidbody>().AddForce(cam.transform.forward * HookSpeed, ForceMode.Impulse);
-        Destroy(clone, 2);
+        Destroy(clone, 3);*/
+
+
+
     }
 }
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -15,6 +15,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
     public Rigidbody rb;
+    public bool isHooked;
 
     [Header("Movement Stats")]
     [SerializeField]
@@ -41,26 +42,35 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
 
+        //isHooked
+        GameObject go = GameObject.Find("Player");
+        GrapplingHook sc = go.GetComponent<GrapplingHook>();
+        isHooked = sc.isHooked;
+
         #region Jumping
         //jump
-        if (isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0 && !isHooked)
         {
             velocity.y = -10f;
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded && !isHooked)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
         #endregion
 
-        //gravity
+        #region gravity
+        if(!isHooked)
+        {
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        }
+        #endregion
 
         #region walking
 
-        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded && !isHooked)
         {
             if(speed < 16)
             {
@@ -68,7 +78,7 @@ public class ThirdPersonMovement : MonoBehaviour
             }
         }
 
-        if (!Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        if (!Input.GetKey(KeyCode.LeftShift) && isGrounded && !isHooked)
         {
             if (speed > 8)
             {
@@ -85,7 +95,7 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f && !isHooked)
         {
 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
